d02df58dc93c80018bf75c58f80fbd3c6dd1486c
christsondev <christson.dev@gmail.com>
2025-04-01 19:54:57 +0800

    Utilities: Modify Date logic

--- mod-utilities/src/main/java/com/christsondev/utilities/Date.kt
+++ mod-utilities/src/main/java/com/christsondev/utilities/Date.kt
@@ -2,29 +2,40 @@ package com.christsondev.utilities
 
 import android.icu.util.Calendar
 import java.text.SimpleDateFormat
-import java.util.Date
 import java.util.Locale
 
-class Date(val millis: Long = Calendar.getInstance().timeInMillis) {
+class Date(val timeInMillis: Long) {
 
-    fun toDayMonthYearDash(): String =
-        if (millis > 0) {
-            SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date(millis))
+    /**
+     * @param separator any value e.g: slash `/` or comma `,` or dash `-`
+     * @return dd<separator>MM<separator>yyyy e.g: dd-MM-yyyy or dd/MM/yyyy
+     */
+    fun toDayMonthYear(separator: String): String =
+        if (timeInMillis > 0) {
+            SimpleDateFormat("dd" + separator + "MM" + separator + "yyyy", Locale.getDefault()).format(Date(timeInMillis))
         } else {
             ""
         }
 
-    fun toDayMonthYearSlash(): String =
-        if (millis > 0) {
-            SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(Date(millis))
-        } else {
-            ""
-        }
-
+    /**
+     * @return hh:mm a
+     */
     fun toTime(): String =
-        if (millis > 0) {
-            SimpleDateFormat("hh:mm a", Locale.getDefault()).format(Date(millis))
+        if (timeInMillis > 0) {
+            SimpleDateFormat("hh:mm a", Locale.getDefault()).format(Date(timeInMillis))
         } else {
             ""
         }
+
+    fun add(millis: Long) = Date(timeInMillis + millis)
+
+    fun minus(millis: Long) = Date(timeInMillis - millis)
+
+    fun isBefore(date: Date) = this.timeInMillis < date.timeInMillis
+
+    fun isAfter(date: Date) = this.timeInMillis > date.timeInMillis
+
+    companion object {
+        fun now() = Date(Calendar.getInstance().timeInMillis)
+    }
 }
