0501fdfb9c0c8fdea27639127e4df7a90b140991
christsondev <christson.dev@gmail.com>
2025-06-29 17:46:28 +0800

    Added new functions for AppDate

--- mod-utilities/src/main/java/com/christsondev/utilities/AppDate.kt
+++ mod-utilities/src/main/java/com/christsondev/utilities/AppDate.kt
@@ -1,13 +1,13 @@
 package com.christsondev.utilities
 
-import android.icu.util.Calendar
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
 class AppDate(val timeInMillis: Long) {
 
-    fun format(format: DateFormat, locale: Locale = Locale.getDefault()) =
+    fun format(format: Formatter, locale: Locale = Locale.getDefault()) =
         if (timeInMillis > 0) {
             SimpleDateFormat(format.get(), locale).format(Date(timeInMillis))
         } else {
@@ -18,26 +18,91 @@ class AppDate(val timeInMillis: Long) {
 
     fun minus(millis: Long) = AppDate(timeInMillis - millis)
 
-    fun isBefore(date: AppDate) = this.timeInMillis < date.timeInMillis
+    fun toCalendar() = Calendar.getInstance().apply { timeInMillis = this@AppDate.timeInMillis }
 
-    fun isAfter(date: AppDate) = this.timeInMillis > date.timeInMillis
+    private fun isBefore(date: AppDate, unit: TimeUnit = TimeUnit.MILLIS): Boolean {
+        return when (unit) {
+            TimeUnit.MILLIS -> this.timeInMillis < date.timeInMillis
+            TimeUnit.SECONDS -> (this.timeInMillis / 1000) < (date.timeInMillis / 1000)
+            TimeUnit.MONTHS -> {
+                val currentCalender = this.toCalendar()
+                val otherCalendar = date.toCalendar()
+
+                val thisYear = currentCalender.get(Calendar.YEAR)
+                val otherYear = otherCalendar.get(Calendar.YEAR)
+                val thisMonth = currentCalender.get(Calendar.MONTH) // Calendar.MONTH is 0-indexed
+                val otherMonth = otherCalendar.get(Calendar.MONTH)
+
+                if (thisYear < otherYear) {
+                    true
+                } else if (thisYear > otherYear) {
+                    false
+                } else {
+                    thisMonth < otherMonth
+                }
+            }
+            TimeUnit.YEARS -> {
+                this.toCalendar().get(Calendar.YEAR) < date.toCalendar().get(Calendar.YEAR)
+            }
+        }
+    }
+
+    private fun isAfter(date: AppDate, unit: TimeUnit = TimeUnit.MILLIS): Boolean {
+        return when (unit) {
+            TimeUnit.MILLIS -> this.timeInMillis > date.timeInMillis
+            TimeUnit.SECONDS -> (this.timeInMillis / 1000) > (date.timeInMillis / 1000)
+            TimeUnit.MONTHS -> {
+                val currentCalender = this.toCalendar()
+                val otherCalendar = date.toCalendar()
+
+                val thisYear = currentCalender.get(Calendar.YEAR)
+                val otherYear = otherCalendar.get(Calendar.YEAR)
+                val thisMonth = currentCalender.get(Calendar.MONTH) // Calendar.MONTH is 0-indexed
+                val otherMonth = otherCalendar.get(Calendar.MONTH)
+
+                if (thisYear > otherYear) {
+                    true
+                } else if (thisYear < otherYear) {
+                    false
+                } else {
+                    thisMonth > otherMonth
+                }
+            }
+            TimeUnit.YEARS -> {
+                this.toCalendar().get(Calendar.YEAR) > date.toCalendar().get(Calendar.YEAR)
+            }
+        }
+    }
+
+    fun isBeforeOrEquals(date: AppDate, timeUnit: TimeUnit = TimeUnit.MILLIS) =
+        !isAfter(date, timeUnit)
+
+    fun isAfterOrEquals(date: AppDate, timeUnit: TimeUnit = TimeUnit.MILLIS) =
+        !isBefore(date, timeUnit)
 
     companion object {
         fun now() = AppDate(Calendar.getInstance().timeInMillis)
     }
 
-    sealed interface DateFormat {
+    enum class TimeUnit {
+        MILLIS,
+        SECONDS,
+        MONTHS,
+        YEARS
+    }
+
+    sealed interface Formatter {
         fun get(): String
 
-        data class NumericMonth(val separator: String) : DateFormat {
+        data class NumericMonth(val separator: String) : Formatter {
             override fun get() = "dd" + separator + "MM" + separator + "yyyy"
         }
 
-        data class TextMonth(val separator: String) : DateFormat {
+        data class TextMonth(val separator: String) : Formatter {
             override fun get() = "dd" + separator + "MMM" + separator + "yyyy"
         }
 
-        data object StandardTime : DateFormat {
+        data object StandardTime : Formatter {
             override fun get() = "hh:mm a"
         }
     }
